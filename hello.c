60136be0c9fb1dec15fa0668c7ef60aa28c16483
Jéssica Wellen <jwellenleites@gmail.com>
2017-08-11 20:52:32 -0300

    Hello com vários argumentos

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int main(int argc, char** argv)
 {
   printf("Como o programa foi chamado: %s\n", argv[0]);
   if (argc > 1)
-    printf("Hello world %s!\n", argv[1]);
+    for (int i=1; i<argc; i++)
+      printf("Hello world %s!\n", argv[i]);
   else
    {
      printf("Hello world!\n");
