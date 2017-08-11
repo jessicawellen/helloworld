01df55203e352d5da13c8ce3a72e6a1f568c5973
Jéssica Wellen <jwellenleites@gmail.com>
2017-08-11 20:46:42 -0300

    Hello com argumentos (com @jurandysoares)

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  printf("Hello world!\n");
+  printf("Como o programa foi chamado: %s\n", argv[0]);
+  if (argc > 1)
+    printf("Hello world %s!\n", argv[1]);
+  else
+   {
+     printf("Hello world!\n");
+     printf("Please, inform you name as an argument.\n");
+   }
+  
+  return 0;
 }
